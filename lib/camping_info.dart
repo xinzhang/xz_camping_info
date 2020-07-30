f485f28831c5ed832a5890c8e2c632cb95f6a0ac
Xin Zhang <xinzhang@192-168-1-125.tpgi.com.au>
2020-07-30 22:10:15 +1000

    init draft

---
+++ lib/camping_info.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:xz_camping_info/components/text_section.dart';
+
+import 'components/title_section.dart';
+
+class CampingInfo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    Color color = Theme.of(context).primaryColor;
+
+    return Scaffold(
+      appBar: AppBar(title: Text("Camping info")),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Image.asset(
+              'assets/images/lakes.jpg',
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
+            ),
+            TitleSection(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                buildButtonColumn(color, Icons.call, "CALL"),
+                buildButtonColumn(color, Icons.near_me, "ROUTE"),
+                buildButtonColumn(color, Icons.share, "SHARE"),
+              ],
+            ),
+            TextSection()
+          ],
+        ),
+      ),
+    );
+  }
+
+  Column buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(icon, color: color),
+        Container(
+            margin: const EdgeInsets.only(top: 8),
+            child: Text(
+              label,
+              style: TextStyle(
+                  fontSize: 12, fontWeight: FontWeight.w400, color: color),
+            ))
+      ],
+    );
+  }
+}
---
+++ lib/components/title_section.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class TitleSection extends StatelessWidget {
+  const TitleSection({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Text("Oeschinen Lake Campground",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 14)),
+                ),
+                Text("Kanderste, Switzeland",
+                    style: TextStyle(color: Colors.grey[500]))
+              ],
+            ),
+          ),
+          Icon(Icons.star, color: Colors.red[500]),
+          Text('41'),
+        ],
+      ),
+    );
+  }
+}
